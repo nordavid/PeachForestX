7fad2dc67c16873ea92f4078cb0ca30507ec07bd
David Einenkel <david.einenkel@gmail.com>
2022-11-10 15:56:34 +0100

    Code optimization

--- src/main/java/de/davideinenkel/peachforestx/menusystem/menus/TradingMenu.java
+++ src/main/java/de/davideinenkel/peachforestx/menusystem/menus/TradingMenu.java
@@ -4,7 +4,6 @@ import de.davideinenkel.peachforestx.menusystem.Menu;
 import de.davideinenkel.peachforestx.menusystem.PlayerMenuUtility;
 import de.davideinenkel.peachforestx.trading.Trade;
 import de.davideinenkel.peachforestx.utility.CustomHead;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -18,12 +17,17 @@ import java.util.Map;
 public class TradingMenu extends Menu {
     Trade trade;
 
+    Trade.Type menuType;
+
     Integer[] forbiddenSlots = {4,13,22,27,28,29,30,31,32,33,34,35,42,43,44};
     Integer[] droppableSlots = {1,2,3,10,11,12,19,20,21};
 
     public TradingMenu(PlayerMenuUtility playerMenuUtility, Trade trade) {
         super(playerMenuUtility);
         this.trade = trade;
+
+        if(playerMenuUtility.getOwner() == trade.getHost()) menuType = Trade.Type.HOST;
+        else menuType = Trade.Type.TARGET;
     }
 
     @Override
@@ -48,20 +52,20 @@ public class TradingMenu extends Menu {
 
         Player player = (Player) e.getWhoClicked();
 
-        if(clickedSlot == 43 && !trade.getLockStateForPlayer(player)) {
+        if(clickedSlot == 43 && trade.getTradeStateForPlayer(menuType) == Trade.TradeState.TRADING) {
             inventory.setItem(43, new ItemStack(Material.AIR));
             trade.lockTrade(player);
             return;
-        } else if (clickedSlot == 43 && trade.getTradeState() == Trade.TradeState.READYTOTRADE) {
+        } else if (clickedSlot == 43 && trade.haveBothLocked()) {
             player.sendMessage("trraaadddding");
-            trade.tradeAccepted();
+            trade.acceptTrade(player);
             return;
         } else if (clickedSlot == 44) {
             trade.tradeDeclined();
             return;
         }
 
-        if(trade.getLockStateForPlayer(player)) {
+        if(trade.getTradeStateForPlayer(menuType) != Trade.TradeState.TRADING) {
             e.setCancelled(true);
             return;
         }
@@ -75,12 +79,9 @@ public class TradingMenu extends Menu {
         // Place item in drop area
         if(Arrays.stream(droppableSlots).anyMatch(i -> i == clickedSlot)) {
             if(action == InventoryAction.PLACE_ALL || action == InventoryAction.PLACE_ONE || action == InventoryAction.PLACE_SOME || action == InventoryAction.SWAP_WITH_CURSOR) {
-                //player.sendMessage("Amount: " + e.getCursor().getAmount());
-                trade.addItem(clickedSlot, e.getCursor().clone(), isHost() ? Trade.Type.HOST : Trade.Type.TARGET);
-                trade.updateTradingMenus(isHost() ? Trade.Type.TARGET : Trade.Type.HOST);
+                trade.addItem(clickedSlot, e.getCursor().clone(), menuType);
             } else if(action == InventoryAction.PICKUP_ALL) {
-                trade.removeItem(clickedSlot, e.getCurrentItem(), isHost() ? Trade.Type.HOST : Trade.Type.TARGET);
-                trade.updateTradingMenus(isHost() ? Trade.Type.TARGET : Trade.Type.HOST);
+                trade.removeItem(clickedSlot, e.getCurrentItem(), menuType);
             }
         }
 
@@ -89,51 +90,12 @@ public class TradingMenu extends Menu {
 
     @Override
     public void setMenuItems() {
-        for (int i = 27; i <= 35; i++) {
-            if (inventory.getItem(i) == null) {
-                inventory.setItem(i, super.FILLER_GLASS);
-            }
-        }
-        inventory.setItem(4, super.FILLER_GLASS);
-        inventory.setItem(13, super.FILLER_GLASS);
-        inventory.setItem(22, super.FILLER_GLASS);
-        inventory.setItem(42, super.FILLER_GLASS);
 
-        inventory.setItem(26, super.FILLER_GLASS);
-
-        if(isHost() && trade.getLockStateForPlayer(trade.getHost())) {
-            inventory.setItem(18, new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1));
-        }
-        else if (isHost() && !trade.getLockStateForPlayer(trade.getHost())){
-            inventory.setItem(18, new ItemStack(Material.RED_STAINED_GLASS_PANE, 1));
-        }
-
-        if(!isHost() && trade.getLockStateForPlayer(trade.getTarget())) {
-            inventory.setItem(18, new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1));
-        }
-        else if (!isHost() && !trade.getLockStateForPlayer(trade.getTarget())){
-            inventory.setItem(18, new ItemStack(Material.RED_STAINED_GLASS_PANE, 1));
-        }
-
-
-        if(isHost() && trade.getLockStateForPlayer(trade.getTarget())) {
-            inventory.setItem(26, new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1));
-        }
-        else if (isHost() && !trade.getLockStateForPlayer(trade.getTarget())){
-            inventory.setItem(26, new ItemStack(Material.RED_STAINED_GLASS_PANE, 1));
-        }
-
-        if(!isHost() && trade.getLockStateForPlayer(trade.getHost())) {
-            inventory.setItem(26, new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1));
-        }
-        else if (!isHost() && !trade.getLockStateForPlayer(trade.getHost())){
-            inventory.setItem(26, new ItemStack(Material.RED_STAINED_GLASS_PANE, 1));
-        }
+        addTradingGui();
+        addLockedIndicators();
 
         //ItemStack accept = new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1);
         //ItemStack decline = new ItemStack(Material.RED_STAINED_GLASS_PANE, 1);
-
-        //https://textures.minecraft.net/texture/8a59084f659df462667d6ac711fc862d8cc5b0ee49bd4d50e85bb54c28a946f4
 
         inventory.setItem(0, CustomHead.getPlayerHead(playerMenuUtility.getOwner(), playerMenuUtility.getOwner().getDisplayName()));
         inventory.setItem(8, CustomHead.getPlayerHead(isHost() ? trade.getTarget() : trade.getHost(), isHost() ? trade.getTarget().getDisplayName() : trade.getHost().getDisplayName()));
@@ -143,6 +105,9 @@ public class TradingMenu extends Menu {
         inventory.setItem(9, peach);
         inventory.setItem(17, peach);
 
+        ItemStack clock = CustomHead.getPlayerHead("https://textures.minecraft.net/texture/8a59084f659df462667d6ac711fc862d8cc5b0ee49bd4d50e85bb54c28a946f4");
+        clock = addMetaToItem(clock, "§7§lWarte...");
+
         ItemStack lock = CustomHead.getPlayerHead("https://textures.minecraft.net/texture/c6e45d7cf4ff21732172d71b2a340626c708397626319b6d57d76c9ac48c675f");
         lock = addMetaToItem(lock, "§a§lAngebot locken");
         ItemStack accept = CustomHead.getPlayerHead("https://textures.minecraft.net/texture/a92e31ffb59c90ab08fc9dc1fe26802035a3a47c42fee63423bcdb4262ecb9b6");
@@ -150,9 +115,11 @@ public class TradingMenu extends Menu {
         ItemStack decline = CustomHead.getPlayerHead("https://textures.minecraft.net/texture/3c4d7a3bc3de833d3032e85a0bf6f2bef7687862b3c6bc40ce731064f615dd9d");
         decline = addMetaToItem(decline, ChatColor.RED + "" + ChatColor.BOLD + "Abbrechen");
 
-        if(trade.getTradeState() == Trade.TradeState.READYTOTRADE) {
+        if(trade.haveBothLocked()) {
             inventory.setItem(43, accept);
-        } else if (!trade.getLockStateForPlayer(playerMenuUtility.getOwner())) {
+        } else if (trade.getTradeStateForPlayer(menuType) == Trade.TradeState.READYTOTRADE) {
+            inventory.setItem(43, clock);
+        } else if (trade.getTradeStateForPlayer(menuType) == Trade.TradeState.TRADING) {
             inventory.setItem(43, lock);
         }
 
@@ -163,7 +130,6 @@ public class TradingMenu extends Menu {
             Integer slot = entry.getKey();
             ItemStack is = entry.getValue();
             if(!isHost()) slot = getInvertedSlot(slot);
-            Bukkit.getLogger().info("xDS slot: " + slot + " item " + is.getType());
 
             inventory.setItem(slot, is);
         }
@@ -173,9 +139,32 @@ public class TradingMenu extends Menu {
             Integer slot = entry.getKey();
             ItemStack is = entry.getValue();
             if(isHost()) slot = getInvertedSlot(slot);
-            Bukkit.getLogger().info("xDSt slot: " + slot);
+
             inventory.setItem(slot, is);
         }
+    }
+
+    private void addTradingGui() {
+        for (int i = 27; i <= 35; i++) {
+            if (inventory.getItem(i) == null) {
+                inventory.setItem(i, super.FILLER_GLASS);
+            }
+        }
+        inventory.setItem(4, super.FILLER_GLASS);
+        inventory.setItem(13, super.FILLER_GLASS);
+        inventory.setItem(22, super.FILLER_GLASS);
+        inventory.setItem(42, super.FILLER_GLASS);
+        inventory.setItem(26, super.FILLER_GLASS);
+    }
+
+    private void addLockedIndicators() {
+        Trade.TradeState tradeState = trade.getTradeStateForPlayer(menuType);
+        if(tradeState == Trade.TradeState.LOCKED) inventory.setItem(18, makeItem(Material.LIME_STAINED_GLASS_PANE, ""));
+        else inventory.setItem(18, makeItem(Material.RED_STAINED_GLASS_PANE, ""));
+
+        tradeState = trade.getTradeStateForPlayer(menuType == Trade.Type.HOST ? Trade.Type.TARGET : Trade.Type.HOST);
+        if(tradeState == Trade.TradeState.LOCKED) inventory.setItem(26, makeItem(Material.LIME_STAINED_GLASS_PANE, ""));
+        else inventory.setItem(26, makeItem(Material.RED_STAINED_GLASS_PANE, ""));
     }
 
     public Integer getInvertedSlot(Integer slot) {
--- src/main/java/de/davideinenkel/peachforestx/trading/Trade.java
+++ src/main/java/de/davideinenkel/peachforestx/trading/Trade.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Trade {
     public static HashMap<Player, Player> tradeRequests = new HashMap<>();
@@ -20,7 +19,6 @@ public class Trade {
     public enum TradeState {
         TRADING,
         LOCKED,
-        WAITING,
         READYTOTRADE
     }
 
@@ -29,13 +27,11 @@ public class Trade {
     private TradingMenu hostMenu;
     private TradingMenu targetMenu;
 
-    private Boolean hasHostLocked = false;
-    private Boolean hasTargetLocked = false;
+    private TradeState hostTradeState = TradeState.TRADING;
+    private TradeState targetTradeState = TradeState.TRADING;
 
     private HashMap<Integer, ItemStack> hostItems = new HashMap<>();
     private HashMap<Integer, ItemStack> targetItems = new HashMap<>();
-
-    private TradeState tradeState = TradeState.TRADING;
 
     public Trade(Player host, Player target) {
         this.host = host;
@@ -46,31 +42,28 @@ public class Trade {
         targetMenu.open();
     }
 
-    public void updateTradingMenus() {
-        hostMenu.open();
-        targetMenu.open();
+    public Player getHost() {
+        return host;
     }
 
-    public void updateTradingMenus(Type type) {
-        if(type == Type.TARGET) targetMenu.open();
-        if(type == Type.HOST) hostMenu.open();
+    public Player getTarget() {return target;}
+
+    public void acceptTrade(Player player) {
+        if(player == host) hostTradeState = TradeState.READYTOTRADE;
+        else targetTradeState = TradeState.READYTOTRADE;
+
+        if(hostTradeState == TradeState.READYTOTRADE && targetTradeState == TradeState.READYTOTRADE) executeTrade();
     }
 
-    public void tradeAccepted() {
+    private void executeTrade() {
         host.closeInventory();
         target.closeInventory();
 
-        // Place Host Items
-        for(Map.Entry<Integer, ItemStack> entry : hostItems.entrySet()) {
-            Integer slot = entry.getKey();
-            ItemStack is = entry.getValue();
+        for (ItemStack is : hostItems.values()) {
             target.getInventory().addItem(is);
         }
 
-        // Place Target Items
-        for(Map.Entry<Integer, ItemStack> entry : targetItems.entrySet()) {
-            Integer slot = entry.getKey();
-            ItemStack is = entry.getValue();
+        for (ItemStack is : targetItems.values()) {
             host.getInventory().addItem(is);
         }
     }
@@ -79,57 +72,41 @@ public class Trade {
         host.closeInventory();
         target.closeInventory();
 
-        // Place Host Items
-        for(Map.Entry<Integer, ItemStack> entry : hostItems.entrySet()) {
-            Integer slot = entry.getKey();
-            ItemStack is = entry.getValue();
+        for (ItemStack is : hostItems.values()) {
             host.getInventory().addItem(is);
         }
 
-        // Place Target Items
-        for(Map.Entry<Integer, ItemStack> entry : targetItems.entrySet()) {
-            Integer slot = entry.getKey();
-            ItemStack is = entry.getValue();
+        for (ItemStack is : targetItems.values()) {
             target.getInventory().addItem(is);
         }
     }
 
-    public Player getHost() {
-        return host;
-    }
-
-    public Player getTarget() {return target;}
-
     public void lockTrade(Player player) {
-        if(player == host) hasHostLocked = true;
-        else hasTargetLocked = true;
-        checkIfTradeIsReady();
+        if(player == host) hostTradeState = TradeState.LOCKED;
+        else targetTradeState = TradeState.LOCKED;
+
         updateTradingMenus();
     }
 
-    private void checkIfTradeIsReady() {
-        if(hasHostLocked && hasTargetLocked) {
-            tradeState = TradeState.READYTOTRADE;
-            updateTradingMenus();
-        }
+    public Boolean haveBothLocked() {
+        if(hostTradeState == TradeState.LOCKED && targetTradeState == TradeState.LOCKED) return true;
+        else return false;
     }
 
-    public Boolean getLockStateForPlayer(Player player) {
-        if(player == host) return hasHostLocked;
-        else return hasTargetLocked;
-    }
-
-    public TradeState getTradeState() {
-        return tradeState;
+    public TradeState getTradeStateForPlayer(Type type) {
+        if(type == Type.HOST) return hostTradeState;
+        else return targetTradeState;
     }
 
     public void addItem(Integer slot, ItemStack item, Type type) {
         Bukkit.getLogger().info("item added");
         if(type == Type.HOST) {
             if(!hostItems.containsKey(slot)) hostItems.put(slot, item);
+            updateTradingMenus(Type.TARGET);
         }
         if(type == Type.TARGET) {
             if(!targetItems.containsKey(slot)) targetItems.put(slot, item);
+            updateTradingMenus(Type.HOST);
         }
     }
 
@@ -146,5 +123,15 @@ public class Trade {
         if(type == Type.HOST) return hostItems;
         if(type == Type.TARGET) return targetItems;
         return null;
+    }
+
+    public void updateTradingMenus() {
+        hostMenu.open();
+        targetMenu.open();
+    }
+
+    public void updateTradingMenus(Type type) {
+        if(type == Type.TARGET) targetMenu.open();
+        if(type == Type.HOST) hostMenu.open();
     }
 }
